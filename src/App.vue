44b22df9776891c496071a8974eed1a40a6338e2
storemalt <storemaltinside@gmail.com>
2019-10-12 23:04:48 +0800

    feat(vuetify): added vuetify ui framework and prepare some components

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,34 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="main">
+    <!-- START::Navigation Bar -->
+    <nav-bar></nav-bar><!-- END::Navigation Bar -->
+
+    <!--START::Content-->
+    <v-content>
+      <v-container
+        fluid
+      >
+        <router-view></router-view>
+      </v-container>
+    </v-content><!--END::Content-->
+
+    <!-- START::Footer Bar -->
+    <footer-bar></footer-bar><!-- END::Footer Bar -->
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
+  import NavBar from '@/components/layouts/NavBar.vue';
+  import FooterBar from '@/components/layouts/FooterBar.vue';
+
+  @Component({
+    components: {
+      NavBar,
+      FooterBar,
+    },
+  })
+
+  export default class App extends Vue {}
+</script>
+
